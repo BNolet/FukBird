495b350bc16aa541c1fbfd0dc398f54b046dff31
Andreas Stjerneklar Kristensen <andreas.kristensen@gmail.com>
2016-04-10 11:05:39 +0200

    Update rlcs.user.js

--- rlcs.user.js
+++ rlcs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RLCS
 // @namespace    http://tampermonkey.net/
-// @version      0.9 - Revamp 
+// @version      0.9 Revamp 
 // @description  Chat-like functionality for Reddit Live
 // @author       FatherDerp, Stjerneklar
 // @include      https://www.reddit.com/live/*
@@ -30,6 +30,7 @@ $('.main-content aside.sidebar').appendTo('#rlc-sidebar');
         $(".usertext-edit.md-container textarea").focus().val("").val(source + " " + username + " ");
     });
  
+});
  /*add css styles, every line must end with \  */
       GM_addStyle(" \
         .liveupdate-listing li.liveupdate ul.buttonrow { display: none!important;} \
@@ -45,86 +46,85 @@ $('.main-content aside.sidebar').appendTo('#rlc-sidebar');
         aside.sidebar {background:#404040;!important} \
         h2{color:white!important} \
         #new-update-form .usertext {  max-width: none;} \
-.usertext-edit .md {min-width: 100%!important;} \
-.content { \
-    width: 95%; \
-    position:relative; \
-} \
-header#liveupdate-header { \
-    position: absolute; \
-    right: 10px; \
-    top:10px; \
-    width: 305px; \
-} \
-div#new-update-form { \
-    position: absolute; \
-    bottom: 5px; \
-    background: #161616; \
-    width: 74%; \
-    margin: 0; \
-    left: 10px; \
-} \
-.main-content { \
-    margin-top: 0; \
-    width: 100%; \
-} \
-aside.sidebar.side.md-container { \
-    position: absolute; \
-    right: 0; \
-    top: 190px; \
-    padding:10px; \
-} \
-ol.liveupdate-listing { \
-    max-width: 76%; \
-    overflow-y: scroll; \
-    padding-right: 15px; \
-    height: calc(100vh - 250px); \
-    margin-bottom: 150px; \
-} \
-.liveupdate-listing li.liveupdate {  border-top:1px solid grey;padding-top:2px; } \
-.liveupdate-listing li.liveupdate .body div.md { \
-    width: 84%; \
-    display: block; \
-    float: right; \
-    margin-bottom:0; \
-    max-width: none; \
-} \
-a.author { \
-    display: block; \
-    width: 15%; \
-    float: left; \
-    } \
-.liveupdate-listing li.liveupdate time { \
-    padding: 0; \
-    width: 15%; \
-    float:left; \
-} \
-.usertext-edit.md-container { \
-    max-width: 100%; \
-    margin: 0; \
-} \
-ol.liveupdate-listing { \
-    display: flex; \
-    flex-direction: column-reverse; \
-} \
-.liveupdate-listing li.liveupdate {height: auto!important;overflow:visible;} \
-.footer-parent {    display: none; } \
-#rlc-main { \ 
-    width: 70%; \
-    height: 100%;\
-    position: fixed;\
-    top: 63px;\
-    left:0;\
-}\
-#rlc-sidebar {\
-    width: 30%;\
-    height: 100%;\
-    position: fixed;\
-    top: 63px;\
-    right:0;\
-  display:block;\
-}\
+        .usertext-edit .md {min-width: 100%!important;} \
+        .content { \
+            width: 95%; \
+            position:relative; \
+        } \
+        header#liveupdate-header { \
+            position: absolute; \
+            right: 10px; \
+            top:10px; \
+            width: 305px; \
+        } \
+        div#new-update-form { \
+            position: absolute; \
+            bottom: 5px; \
+            background: #161616; \
+            width: 74%; \
+            margin: 0; \
+            left: 10px; \
+        } \
+        .main-content { \
+            margin-top: 0; \
+            width: 100%; \
+        } \
+        aside.sidebar.side.md-container { \
+            position: absolute; \
+            right: 0; \
+            top: 190px; \
+            padding:10px; \
+        } \
+        ol.liveupdate-listing { \
+            max-width: 76%; \
+            overflow-y: scroll; \
+            padding-right: 15px; \
+            height: calc(100vh - 250px); \
+            margin-bottom: 150px; \
+        } \
+        .liveupdate-listing li.liveupdate {  border-top:1px solid grey;padding-top:2px; } \
+        .liveupdate-listing li.liveupdate .body div.md { \
+            width: 84%; \
+            display: block; \
+            float: right; \
+            margin-bottom:0; \
+            max-width: none; \
+        } \
+        a.author { \
+            display: block; \
+            width: 15%; \
+            float: left; \
+            } \
+        .liveupdate-listing li.liveupdate time { \
+            padding: 0; \
+            width: 15%; \
+            float:left; \
+        } \
+        .usertext-edit.md-container { \
+            max-width: 100%; \
+            margin: 0; \
+        } \
+        ol.liveupdate-listing { \
+            display: flex; \
+            flex-direction: column-reverse; \
+        } \
+        .liveupdate-listing li.liveupdate {height: auto!important;overflow:visible;} \
+        .footer-parent {    display: none; } \
+        #rlc-main { \
+            width: 70%; \
+            height: 100%; \
+            position: fixed; \
+            top: 63px; \
+            left:0; \
+        } \
+        #rlc-sidebar { \
+            width: 30%; \
+            height: 100%; \
+            position: fixed; \
+            top: 63px; \
+            right:0; \
+          display:block; \
+        } \
 ");
-});
 
 
